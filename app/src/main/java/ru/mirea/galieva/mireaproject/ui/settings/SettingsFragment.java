ebe31aaf89054223fceda6ac86a76e991b64e9bc
Guzel-Galieva <guzel.vv.com@gmail.com>
2021-06-06 12:50:04 +0300

    new

--- app/src/main/java/ru/mirea/galieva/mireaproject/ui/settings/SettingsFragment.java
+++ app/src/main/java/ru/mirea/galieva/mireaproject/ui/settings/SettingsFragment.java
@@ -54,22 +54,6 @@ public class SettingsFragment extends Fragment implements View.OnClickListener{
         preferences = getActivity().getPreferences(MODE_PRIVATE);
         return view;
     }
-    /*public void onSave() {
-        Log.d("ghjdthrf", "работает");
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString(SAVED_TEXT, currentTheme);
-        editor.apply();
-    }
-    public void onLoad() {
-        String text = preferences.getString(SAVED_TEXT, "light");
-        if(text.equals("light")){
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-        }else if(text.equals("dark")){
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-        }else{
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-        }
-    }*/
 
     @Override
     public void onClick(View v) {
@@ -80,12 +64,12 @@ public class SettingsFragment extends Fragment implements View.OnClickListener{
             case R.id.buttonl:
                 currentTheme = "light";
                 break;
-            case R.id.buttonlo:
+            case R.id.buttonsa:
                 SharedPreferences.Editor editor = preferences.edit();
                 editor.putString(SAVED_TEXT, currentTheme);
                 editor.apply();
                 break;
-            case R.id.buttonsa:
+            case R.id.buttonlo:
                 String text = preferences.getString(SAVED_TEXT, "light");
                 if(text.equals("light")){
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
